40e004327a91311768c4614e968b8b4d367756ab
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-26 22:15:05 -0400

    up cambio de codigo por interpretacion

--- smartwatch1.rb
+++ smartwatch1.rb
@@ -1,9 +1,10 @@
-#pasos = ['100', '21', '231as', '2031', '1052000', '213b', 'b123']
-pasos = [1000, 5000,'100', '21', '231as', 100, 20000,'2031', '1052000', 8000,'213b', 'b123', 50000, 105000]
+pasos = ['100', '21', '231as', '2031', '1052000', '213b', 'b123']
+#pasos = [1000, 5000,'100', '21', '231as', 100, 20000,'2031', '1052000', 8000,'213b', 'b123', 50000, 105000]
 
 def clear_steps arreglo
-    convert = arreglo.map { |x| x.to_i}
-    convert.reject { |x| x < 200 || x > 100000}        
+    convert = arreglo.select { |x| x.scan(/\D/).length < 1 if  x}
+    convert.map! {|x| x.to_i}
+    convert.reject { |x| x.to_i < 200 || x.to_i > 100000}        
 end
 
 print clear_steps(pasos)
