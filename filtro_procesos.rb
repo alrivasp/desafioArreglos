74eacdda43164416a711cf0f2fc93365b3681253
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-26 21:23:45 -0400

    Up simplificado

--- filtro_procesos.rb
+++ filtro_procesos.rb
@@ -3,13 +3,11 @@ num = ARGV[0].to_i
 #Metodo de proceso
 def process dato
     #Leer archivo externo
-    data = open('procesos.data').readlines
-    #Eliminar saltos de linea
-    data.map! {|ele| ele.to_i}
+    data = open('procesos.data').readlines        
     #Filtrar segun condicion
-    result = data.reject {|x| x < 250}    
+    result = data.reject {|x| x.to_i < dato}    
     #Exportar resultado
-    File.write('procesos_filtrados.data', result.join("\n"))
+    File.write('procesos_filtrados.data', result.join)
 end
 #Ejecutar metodo
 process(num)
